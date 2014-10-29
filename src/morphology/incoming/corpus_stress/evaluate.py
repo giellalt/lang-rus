700fb82df40133a17d4b0525a623265823d6b493
fran <fran@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2014-10-29 13:25:03 +0000

    add script
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/rus@101858 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ src/morphology/incoming/corpus_stress/evaluate.py
@@ -0,0 +1,56 @@
+import sys;
+
+r = open(sys.argv[1]);
+t = open(sys.argv[2]);
+
+errors = 0.0;
+tokens = 0.0;
+wins = 0.0;
+
+feiler = [];
+
+while True: #{
+
+	refline = r.readline();
+	tstline = t.readline().replace('\u0300', '');
+
+	if refline == '' or tstline == '': #{
+		break;
+	#}
+
+	refrow = refline.split(' ');
+	tstrow = tstline.split(' ');
+	
+	if len(refrow) != len(tstrow): #{
+		print('ERROR:', file=sys.stderr);
+		break;
+	#}
+	feil = 0.0;
+	for i in range(0, len(tstrow)): #{
+		if refrow[i].count('\u0301') < 1: #{
+			continue;
+		#}
+
+		if tstrow[i] != refrow[i]: #{
+			feil = feil + 1.0;
+			errors = errors + 1.0;
+		else: #{
+			wins = wins + 1.0;
+		#}
+		tokens = tokens + 1.0 ;
+	#}	
+	if feil > 0.0: #{
+		feiler.append((refline, tstline));
+	#}
+#}
+
+for feilline in feiler: #{
+	print('- %s\n+ %s\n' % (feilline[0].strip(), feilline[1].strip()));
+#}
+
+print(errors);
+print(wins);
+print(tokens);
+
+print('acc: %.2f' % (wins/tokens*100.0));
+

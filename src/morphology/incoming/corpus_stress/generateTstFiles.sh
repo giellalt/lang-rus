c567657728f8f4da85f903cfa646f12e59092bda
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2014-12-01 10:59:53 +0000

    restructure script to match new directory structure

--- src/morphology/incoming/corpus_stress/generateTstFiles.sh
+++ src/morphology/incoming/corpus_stress/generateTstFiles.sh
@@ -30,10 +30,10 @@ do
 # TODO pipeline
 #       .GTnoCGsure - Giellatekno FST > resolve stress-irrelevant ambiguity, abstain from remaining ambiguity
   echo -e "\tgenerating $name.GTnoCGsure.tst ..."
-  cat $name.src  | apertium-destxt | hfst-proc -w $GTROOT/langs/rus/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol 2>/dev/null | python3.4 show-all-rus.py $GTROOT/langs/rus/tools/mt/apertium/generator-mt-apertium-norm.hfstol | apertium-retxt > $name.GTnoCGsure.tst
+  cat $name.src  | apertium-destxt | hfst-proc -w $GTROOT/langs/rus/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol 2>/dev/null | python3.4 show-all-rus.py $GTROOT/langs/rus/tools/mt/apertium/generator-mt-apertium-norm.hfstol | apertium-retxt > test_files/$name.GTnoCGsure.tst
 #       .GTCGguess - Giellatekno FST > CG > blindly select the first reading
   echo -e "\tgenerating $name.GTCGguess.tst ..."
-  cat $name.src  | apertium-destxt | hfst-proc -w $GTROOT/langs/rus/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol 2>/dev/null | cg-proc -n -1 -w $ALANGS/apertium-rus/rus.rlx.bin  | $SED 's/<@[A-Za-z→←]\+>//g' | hfst-proc -n $GTROOT/langs/rus/tools/mt/apertium/generator-mt-apertium-norm.hfstol | apertium-retxt > $name.GTCGguess.tst
+  cat $name.src  | apertium-destxt | hfst-proc -w $GTROOT/langs/rus/tools/mt/apertium/analyser-mt-apertium-desc.und.hfstol 2>/dev/null | cg-proc -n -1 -w $ALANGS/apertium-rus/rus.rlx.bin  | $SED 's/<@[A-Za-z→←]\+>//g' | hfst-proc -n $GTROOT/langs/rus/tools/mt/apertium/generator-mt-apertium-norm.hfstol | apertium-retxt > test_files/$name.GTCGguess.tst
 #       .GTCGsure - Giellatekno FST > CG > resolve stress-irrelevant ambiguity, abstain from remaining ambiguity
 # TODO pipeline
 done
@@ -41,10 +41,10 @@ done
 # concatenate results for each condition
 cat *.ref > all.ref
 cat *.src > all.src
-cat *.GTnoCGguess.tst > all.GTnoCGguess.tst
-cat *.GTnoCGsure.tst > all.GTnoCGsure.tst
-cat *.GTCGguess.tst > all.GTCGguess.tst
-cat *.GTCGsure.tst > all.GTCGsure.tst
-cat *.RG.tst > all.RG.tst
+cat test_files/*.GTnoCGguess.tst > test_files/all.GTnoCGguess.tst
+cat test_files/*.GTnoCGsure.tst > test_files/all.GTnoCGsure.tst
+cat test_files/*.GTCGguess.tst > test_files/all.GTCGguess.tst
+cat test_files/*.GTCGsure.tst > test_files/all.GTCGsure.tst
+cat test_files/*.RG.tst > test_files/all.RG.tst
 
 echo "Done!"

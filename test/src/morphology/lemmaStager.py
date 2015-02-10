b63ae9de2135657c7d525feac9e2253fb1bc8948
robert <robert@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-02-10 08:40:29 +0000

    Fixed the lemma generation tests, and fixed a couple lemma problems that they revealed.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/rus@107044 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ test/src/morphology/lemmaStager.py
@@ -0,0 +1,26 @@
+import sys
+
+# USAGE:
+# $ cat inputfile | python3 lemmaStager.py { noun , adjective , verb }
+
+POS = sys.argv[1]
+if POS == 'noun' :
+	myTagSeqs = []
+	for gender in ['+Msc','+Neu','+Fem'] :
+		for animacy in ['+Inan','+Anim'] :
+			for number in ['+Sg','+Pl'] :
+				myTagSeqs.append('+N'+gender+animacy+number+'+Nom')
+	for animacy in ['+Inan','+Anim','+AnIn'] :
+		myTagSeqs.append('+N+MFN'+animacy+'+Pl+Nom')
+	myTagSeqs.append('+N+MFN+Inan+Pl+Gen')
+elif POS == 'adjective' :
+	myTagSeqs = ['+A+Msc+AnIn+Sg+Nom','+A+Msc+AnIn+Sg+Nom+Fac','+A+Msc+Sg+Pred','+A+Cmpnd']
+elif POS == 'verb' :
+	myTagSeqs = ['+V+Impf+IV+Inf','+V+Impf+TV+Inf','+V+Perf+IV+Inf','+V+Perf+TV+Inf']
+
+for lemma in sys.stdin :
+	for tags in myTagSeqs :
+		sys.stdout.write(lemma.strip()+tags+'\n')
+	sys.stdout.write('myDelimiter\n')
+
+sys.stdout.flush()

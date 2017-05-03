7c248696596e24de4dc4451d12b96bfd0f2a7f3f
Trond Trosterud <trond.trosterud@uit.no>
2017-05-03 11:58:05 +0000

    The commands to copy weighted files in case they are not in stems

---
+++ src/scripts/update-weighted-files.sh
@@ -0,0 +1,4 @@
+
+cp src/morphology/incoming/ulp2017/weights/stems/adjectives.lexc.weight src/morphology/stems/adjectives.lexc 
+cp src/morphology/incoming/ulp2017/weights/stems/verbs.lexc.weight src/morphology/stems/verbs.lexc 
+cp src/morphology/incoming/ulp2017/weights/stems/nouns.lexc.weight src/morphology/stems/nouns.lexc 
